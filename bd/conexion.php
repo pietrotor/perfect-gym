b19d312ca78854f1aa21fed80223277d18eeb5bb
Pietro Torrico <torricopietro@gmail.com>
2022-07-14 20:17:24 -0500

    [update] updated db access

--- bd/conexion.php
+++ bd/conexion.php
@@ -2,11 +2,11 @@
 
   class Conexion{
     public static function Conectar(){
-      define('servidor','localhost');
+      define('servidor','us-cdbr-east-06.cleardb.net');
       // define('nombre_bd','perfect-gym2');
-      define('nombre_bd','perfectgym_final');
-      define('usuario','root');
-      define('password','');
+      define('nombre_bd','heroku_d3b6dc432a58281');
+      define('usuario','baf3a3884f845c');
+      define('password','34c0c617');
       $opciones=array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
       try {
           $conexion=new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
